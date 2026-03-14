64b1f05535bd5d348d73f1863b8df87602169b8f
Chris <chrorvik@Chris-Air.localdomain>
2026-03-14 21:13:40 +0100

    nyheter

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
       studioBasePath: "/studio",
       stega: {
         studioUrl: "/studio",
+        enabled: process.env.NODE_ENV !== "production",
       },
     }),
   ],
